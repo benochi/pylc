4c1f33e44f39a97698fb0a7043fa9fa8048aa636
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-09-27 13:57:47 -0500

    merge two sorted arrays.

---
+++ merge.py
@@ -0,0 +1,26 @@
+def merge(nums1, m, nums2, n ):
+  i = m - 1
+  j = n - 1
+  k = n + m -1
+
+  while n >= 0 and j >= 0:
+    if nums1[i] > nums2[j]:
+      nums1[k] = nums1[i]
+      i -= 1
+    else: 
+      nums1[k] = nums2[j]
+      j -= 1
+    k-=1
+    
+  while j >= 0:
+    nums1[k] = nums2[j]
+    j-=1
+    k-=1
+
+
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+merge(nums1, m, nums2, n)
+print(nums1)
