68f4a6abc80a4bc1246b602984b6ca1e53847ab1
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-10-02 12:34:10 -0500

    add instructions.

--- LC/climbingStairs.py
+++ LC/climbingStairs.py
@@ -1,11 +1,17 @@
 # bottom up dynamic approach O(n)
 def climbStairs(n):
+    # set variables for amount of options, stairs 1 or 2 steps only
     one, two = 1, 1
+    # we only want to iterate n-1 times.
     for i in range(n - 1):
+        # store one value
         temp = one
+        # increment one
         one = one + two
+        # set two after addition to new value of one.
         two = temp
 
+    # return total options.
     return one
 
 
