d88fe63e081491199d1fd605b45039d4ee864d9a
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-10-04 09:24:39 -0500

    quickSort done 100% on my own.  boom.

---
+++ sorting/quickSort.py
@@ -0,0 +1,22 @@
+# grab pivot value -> random value usually rightmost
+# pivot = arr[-1]
+# compare every other value to pivot and filter left or right based on comparison <= goes left, > goes right
+
+
+def quickSort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[-1]
+    left = []
+    right = []
+    for i in range(len(arr) - 1):
+        if arr[i] <= pivot:
+            left.append(arr[i])
+        else:
+            right.append(arr[i])
+
+    return quickSort(left) + [pivot] + quickSort(right)
+
+
+print(quickSort([1, 3, 5, 6, 4, 2, 5, 8, 9, 7]))
