a26204106c42a67ff43cb03c423467ab43596557
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-10-07 11:56:43 -0500

    guessNumber LC.

---
+++ LC/guessnumber.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l = 1
+        r = n
+        while l <= r:
+            mid = (l + r) // 2
+            res = guess(mid)
+            if res == 1:
+                l = mid + 1
+            elif res == -1:
+                r = mid - 1
+            else:
+                return mid
