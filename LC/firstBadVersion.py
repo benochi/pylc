0b5b006233ffb7e8e0c9567a2cb7c7cf1c8db4d0
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-10-07 12:23:07 -0500

    firstBadVersion done.

---
+++ LC/firstBadVersion.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l = 1
+        r = n
+
+        while l < r:
+            m = (l + r) // 2
+
+            if isBadVersion(m):
+                r = m
+            else:
+                l = m + 1
+
+        return l
