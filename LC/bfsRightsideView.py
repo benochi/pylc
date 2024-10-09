b1fb51c9387af3d354709bb9b5ec19ff2e4beb4b
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-10-09 08:35:57 -0500

    clarification

--- LC/bfsRightsideView.py
+++ LC/bfsRightsideView.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def rightSideView(self, root):
         """
@@ -22,10 +16,8 @@ class Solution(object):
                     curr = queue.popleft()
                     if i == lastNode:
                         output.append(curr.val)
-
                     if curr.left:
                         queue.append(curr.left)
-
                     if curr.right:
                         queue.append(curr.right)
 
