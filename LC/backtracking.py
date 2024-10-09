c19ac2b951753cdcce9a7e24489dea2d63af0e06
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-10-09 09:20:13 -0500

    backtracking path handling.

--- LC/backtracking.py
+++ LC/backtracking.py
@@ -11,3 +11,19 @@ def canReachLeaf(root):
     if canReachLeaf(root.right):
         return True
     return False
+
+
+# return path without 0 as leaf:
+def leafPath(root, path):
+    if not root or root.val == 0:
+        return False
+    path.append(root.val)
+
+    if not root.left and not root.right:
+        return True
+    if leafPath(root.left, path):
+        return True
+    if leafPath(root.right, path):
+        return True
+    path.pop()
+    return False
