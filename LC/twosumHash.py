d660e62bc7717f114b1eb87f1912d56a9c3a9138
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-10-18 08:02:26 -0500

    2sum hashmap

--- LC/twosumHash.py
+++ LC/twosumHash.py
@@ -1,14 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        count = Counter(nums)
-        print(count)
-        for i, num in enumerate(nums):
-            complement = target - num
-            # Check if the complement exists in the Counter
-            if complement in count:
-                # If complement is the same as num, ensure there are at least 2 occurrences
-                if complement == num and count[complement] > 1:
-                    return [i, nums.index(complement, i + 1)]
-                elif complement != num:
-                    return [i, nums.index(complement)]
-        return []
+        prevMap = {}  # val : index
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
