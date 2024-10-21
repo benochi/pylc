90c0b3e8dbf1c00cd35b9c6ed2c5567594127097
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-10-21 14:34:50 -0500

    notes

--- LC/twosumHash.py
+++ LC/twosumHash.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         prevMap = {}  # val : index
         for i, n in enumerate(nums):
-            diff = target - n
+            diff = target - n  # checking if difference is already in hashmap
             if diff in prevMap:
-                return [prevMap[diff], i]
+                return [prevMap[diff], i]  # return solution if it is.
             prevMap[n] = i
         return
