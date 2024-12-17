2cb757d217e060a03306916bbe0c6f205c53b0d0
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-12-17 07:29:40 -0600

    letter combinations.js

---
+++ amazon/grind169/letterCombinations.js
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) < 1:
+            return []
+
+        letterMap = {
+            '2': ["a", "b", "c"],
+            '3': ["d", "e", "f"],
+            '4': ["g", "h", "i"],
+            '5': ["j", "k", "l"],
+            '6': ["m", "n", "o"],
+            '7': ["p", "q", "r", "s"],
+            '8': ["t", "u", "v"],
+            '9': ["w", "x", "y", "z"]
+            }
+        result = [""]
+        for num in digits:
+            temp = []
+            for combo in result:
+                print(combo)
+                for letter in letterMap[num]:
+                    temp.append(combo + letter)
+            result = temp
+        
+        return result
