b19a991e6e5a823ca2ebf4d2bfe93db1d760511b
Dan Polityka <73758508+benochi@users.noreply.github.com>
2025-01-02 16:42:24 -0600

    minSizeSubarray

---
+++ nc250/minSizeSubarraySum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l = 0
+        currentSum = 0
+        minLength = float('inf')
+
+        for r in range(len(nums)):
+            currentSum += nums[r]
+
+            while currentSum >= target:
+                minLength = min(minLength, r - l + 1)
+                currentSum -= nums[l]
+                left += 1
+        
+        return minLength if minLength != float('inf') else 0
