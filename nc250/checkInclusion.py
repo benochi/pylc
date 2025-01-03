83bbd3dc1b7b615f7411814cc34d95d60310bf79
Dan Polityka <73758508+benochi@users.noreply.github.com>
2025-01-03 08:33:57 -0600

    findK closest, bianry searcha nd sliding window

--- nc250/checkInclusion.py
+++ nc250/checkInclusion.py
@@ -1,22 +1,27 @@
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
+
+def checkInclusion( s1: str, s2: str) -> bool:
+    if len(s1) > len(s2):
+        return False
+    
+    s1Count = [0] * 26
+    window = [0] * 26
+
+    for i in range(len(s1)):
+        s1Count[ord(s1[i]) - ord('a')] += 1
+        window[ord(s2[i]) - ord('a')] += 1
+        print(s1Count)
         
-        s1Count = [0] * 26
-        window = [0] * 26
+    if s1Count == window:
+        return True
 
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            window[ord(s2[i]) - ord('a')] += 1
-
+    for i in range(len(s1), len(s2)):
+        window[ord(s2[i - len(s1)]) - ord('a')] -= 1
+        window[ord(s2[i]) - ord('a')] +=1
+        print(window)
+        print(ord(s2[i - len(s1)]))
+        print(ord(s2[i]) - 97)
         if s1Count == window:
             return True
-
-        for i in range(len(s1), len(s2)):
-            window[ord(s2[i - len(s1)]) - ord('a')] -= 1
-            window[ord(s2[i]) - ord('a')] +=1
-
-            if s1Count == window:
-                return True
-        return False
+    return False
+    
+print(checkInclusion("ad","baedad"))
