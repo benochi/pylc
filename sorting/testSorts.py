90a7f55f0f80c846c2cd159d68023c0a3226627c
Dan Polityka <73758508+benochi@users.noreply.github.com>
2025-01-08 09:50:48 -0600

    more sorts.

--- sorting/testSorts.py
+++ sorting/testSorts.py
@@ -62,6 +62,53 @@ def quickSort(arr):
 
   return quickSort(left) + mid + quickSort(right)
 
+def heapSort(arr):
+    def heapify(arr, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            heapify(arr, n, largest)
+
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
+
+    return arr
+
+def bubbleSort(arr):
+  n = len(arr)
+  for i in range(n):
+      swapped = False
+      for j in range(n - i - 1):
+         if arr[j] > arr[j + 1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            swapped= True
+      if not swapped:
+         break
+  return arr
+            
+def insertionSort(arr):
+  #moves element backwards until it is bigger than i or is arr[0]
+  for i in range(1, len(arr)):
+      tmp = arr[i]
+      j = i - 1
+      while j >= 0 and arr[j] > arr[i]:
+         arr[j + 1] = arr[j]
+         j -= 1
+      arr[j + 1] = tmp
+  return arr
 
 
 for a in testArrs:
@@ -69,3 +116,9 @@ for a in testArrs:
   print("merge ^")
   print(quickSort(a))
   print("quick ^")
+  print(heapSort(a))
+  print("heap ^")
+  print(bubbleSort(a))
+  print("Bubble ^")
+  print(insertionSort(a))
+  print("Insertion ^")
