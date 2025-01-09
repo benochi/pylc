eaa00b027f9324de442b40ab3763bb4e763285c9
Dan Polityka <73758508+benochi@users.noreply.github.com>
2025-01-09 13:35:40 -0600

    add tests

--- primeagen/dfsTree.py
+++ primeagen/dfsTree.py
@@ -10,3 +10,26 @@ def compareTrees(a, b): #a and b are binary trees we're checking to see if they 
   
   return compareTrees(a.left, b.left) and compareTrees(a.right, b.right)
   
+class TreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+# Tree 1
+tree1 = TreeNode(1)
+tree1.left = TreeNode(2)
+tree1.right = TreeNode(3)
+
+# Tree 2
+tree2 = TreeNode(1)
+tree2.left = TreeNode(2)
+tree2.right = TreeNode(3)
+
+# Tree 3 (different structure)
+tree3 = TreeNode(1)
+tree3.left = TreeNode(2)
+
+# Call the function
+print(compareTrees(tree1, tree2)) # True (same shape and size)
+print(compareTrees(tree1, tree3)) # False
